a588fd41969bc508907f72ba7764426a0fa87e15
Sujithkumar S <sujithkumar@Sujithkumars-MacBook-Pro.local>
2023-01-06 13:07:33 +0530

    added a form

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function HomePage(){
+    const [name, setName] = useState('');
+    const [age, setAge] = useState();
+    const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
+    const [gender, setGender] = useState('male');
+    const [plan, setPlan] = useState('$299');
+
     return (
-        <div>
-            <h1> hello world </h1>
+        <div class = "form">
+            <form action = "">
+                <label htmlFor = 'name'>Name</label>
+                <input type = "text" value = { name } onChange = {(e) => setName(e.target.value)} placeholder = 'Enter your Name..'/>
+                <label htmlFor = 'age'>Age</label>
+                <input type = "text" value = { age } onChange = {(e) => setAge(e.target.value)} placeholder = 'Enter your Age..'/>
+                <label htmlFor = 'mail'>Mail</label>
+                <input type = "text" value = { mail } onChange = {(e) => setMail(e.target.value)} placeholder = 'Enter your Mail..'/>
+                <label htmlFor = 'password' >Password</label>
+                <input type = "password" value = { password } onChange = {(e) => setPassword(e.target.value)} placeholder = 'Enter your password..'/>
+                <label htmlFor = 'gender'>Gender</label>
+                <select id = "gender" name = "gender" value = {gender} onChange = {(e) => setGender(e.target.value)}>
+                    <option value = "male" >Male</option>
+                    <option value = "female" >Female</option>
+                </select>
+                <label htmlFor = 'plan'>Membership plan</label>
+                <select id = "plan" name = "plan" value = {plan} onChange = {(e) => setPlan(e.target.value)}>
+                    <option value = "$299">$299</option>
+                    <option value = "$499">$499</option>
+                    <option value = "$999">$999</option>
+                </select>
+            </form>
         </div>
     );
 }

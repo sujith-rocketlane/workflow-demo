48af48b996395573afde5bcc79a66edafba21738
Sujithkumar <sujithkumar@rocketlane.com>
2023-01-18 10:59:22 +0530

    modified name to pascalcase

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -11,7 +11,7 @@ function HomePage(){
     return (
         <div class = "form">
             <form action = "">
-                <label htmlFor = 'name'>name</label>
+                <label htmlFor = 'name'>Name</label>
                 <input type = "text" value = { name } onChange = {(e) => setName(e.target.value)} placeholder = 'Enter your Name..'/>
                 <label htmlFor = 'age'>age</label>
                 <input type = "text" value = { age } onChange = {(e) => setAge(e.target.value)} placeholder = 'Enter your Age..'/>
